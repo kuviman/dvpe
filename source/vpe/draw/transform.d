b8ec5b68fdd16f783fa1e1b583e478d2639a38f0
kuviman <kuviman@gmail.com>
2014-10-10 01:40:52 +0400

    Regular transformations

--- source/vpe/draw/transform.d
+++ source/vpe/draw/transform.d
@@ -2,6 +2,12 @@ module vpe.draw.transform;
 
 import vpe.internal;
 
-void modelMatrix(mat4 mat) {
-	renderState.modelMatrix = mat;
-}
+void modelMatrix(mat4 mat) { renderState.modelMatrix = mat; }
+void multMatrix(mat4 mat) { renderState.modelMatrix = renderState.modelMatrix * mat; }
+
+void rotate(real angle) { multMatrix(mat4.createRotation(vec3(0, 0, 1), angle)); }
+
+void scale(real k) { multMatrix(mat4.createScale(k)); }
+void scale(real kx, real ky, real kz = 1) { multMatrix(mat4.createScale(kx, ky, kz)); }
+
+void translate(real x, real y, real z = 0) { multMatrix(mat4.createTranslation(x, y, z)); }
