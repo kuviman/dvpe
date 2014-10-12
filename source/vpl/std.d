6520b851e60d8ce299793b89d4ba2560d69a02cc
kuviman <kuviman@gmail.com>
2014-10-12 22:26:22 +0400

    vpl.std contains inly true std now

--- source/vpl/std.d
+++ source/vpl/std.d
@@ -11,23 +11,4 @@ import std.traits : CommonType, TypeTuple, isFloatingPoint;
 import std.string : toStringz;
 import std.array : array, split;
 import std.typecons : wrap;
-
 import std.random : uniform, randomShuffle;
-auto random(T)(T lf, T rg) {
-	return uniform(lf, rg);
-}
-auto randomInc(T)(T lf, T rg) {
-	return uniform!"[]"(lf, rg);
-}
-
-template RangeTuple(int from, int to) {
-	static if (from >= to)
-		alias TypeTuple!() RangeTuple;
-	else
-		alias TypeTuple!(from, RangeTuple!(from + 1, to)) RangeTuple;
-}
-alias RangeTuple(int n) = RangeTuple!(0, n);
-
-auto opBinary(string op, T1, T2)(T1 v1, T2 v2) { return mixin("v1 " ~ op ~ " v2"); }
-auto opUnary(string op, T)(T val) { return mixin(op ~ " val"); }
-auto opOpAssign(string op, T1, T2)(ref T1 a, T2 b) { return mixin("a " ~ op ~ "= b"); }
