99f8eb8c96f7b739469c582fb55813c80be9bcb7
kuviman <kuviman@gmail.com>
2014-10-15 23:39:25 +0400

    Calculating FPS

--- source/vpe/clock.d
+++ source/vpe/clock.d
@@ -1,6 +1,7 @@
 module vpe.clock;
 
 import vpe.internal;
+import std.container : DList;
 
 class Clock {
 	private real oldTime;
@@ -12,9 +13,30 @@ class Clock {
 		auto newTime = glfwGetTime();
 		auto dt = newTime - oldTime;
 		oldTime = newTime;
+
+		sumFrames += dt;
+		lastFrames.insertBack(dt);
+		cntFrames++;
+
+		while (cntFrames > NeedFrames) {
+			sumFrames -= lastFrames.front();
+			lastFrames.removeFront();
+			cntFrames--;
+		}
+
 		return dt;
 	}
 	real currentTime() {
 		return glfwGetTime() - startTime;
 	}
+
+	real FPS() {
+		if (cntFrames == 0) return 0;
+		return cntFrames / sumFrames;
+	}
+
+	private size_t cntFrames = 0;
+	private DList!real lastFrames;
+	private enum NeedFrames = 20;
+	private real sumFrames = 0;
 }
