71767e3805cfda516c9d53a756d9dda42dc0fc81
kuviman <kuviman@gmail.com>
2014-10-16 01:37:25 +0400

    vpe.draw.line & rect overloads

--- source/vpe/draw/primitives.d
+++ source/vpe/draw/primitives.d
@@ -21,6 +21,9 @@ void line(vec2 p1, vec2 p2, real width) {
 	draw.quad();
 	draw.load();
 }
+void line(real x1, real y1, real x2, real y2, real width) {
+	line(vec2(x1, y1), vec2(x2, y2), width);
+}
 
 void rect(real x1, real y1, real x2, real y2) {
 	draw.save();
@@ -28,4 +31,7 @@ void rect(real x1, real y1, real x2, real y2) {
 	draw.scale(x2 - x1, y2 - y1);
 	draw.quad();
 	draw.load();
+}
+void rect(vec2 p1, vec2 p2) {
+	rect(p1.x, p1.y, p2.x, p2.y);
 }
