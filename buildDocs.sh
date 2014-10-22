8d076bc7596b2b3bba61757e63775513f0b40159
kuviman <kuviman@gmail.com>
2014-10-23 00:26:13 +0400

    Output only documented stuff

--- buildDocs.sh
+++ buildDocs.sh
@@ -1,6 +1,7 @@
 rm -r docs
 cp -r ~/.dub/packages/ddox-0.10.0/public docs
 dub build --config=buildDocs
+~/.dub/packages/ddox-0.10.0/ddox filter docs.json --only-documented
 ~/.dub/packages/ddox-0.10.0/ddox generate-html docs.json docs
 rm docs.json
 rm __dummy.html
