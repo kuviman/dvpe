79b5fea4b26990a8fc4a4dae874c9160fc5bb2ac
kuviman <kuviman@gmail.com>
2014-11-08 02:16:43 +0400

    No path components in views

--- source/vpe/shader/internal.d
+++ source/vpe/shader/internal.d
@@ -8,17 +8,17 @@ RawShader basicVertexShader;
 Shader colorShader, textureShader, circleShader, dashedShader;
 
 version(OpenGL2)
-	enum gl2 = "/opengl2";
+	enum gl2 = "gl2_";
 else
 	enum gl2 = "";
 
 void initShaders() {
 	quadPoly = new RawPolygon(vec2(0, 0), vec2(1, 0), vec2(1, 1), vec2(0, 1));
-	basicVertexShader = new RawShader(GL_VERTEX_SHADER, import("shader"~gl2~"/vertex/basic.glsl"));
-	colorShader = new Shader(import("shader"~gl2~"/fragment/color.glsl"));
-	textureShader = new Shader(import("shader"~gl2~"/fragment/texture.glsl"));
-	circleShader = new Shader(import("shader"~gl2~"/fragment/circle.glsl"));
-	dashedShader = new Shader(import("shader"~gl2~"/fragment/dashed.glsl"));
+	basicVertexShader = new RawShader(GL_VERTEX_SHADER, import(gl2~"shader_vertex_basic.glsl"));
+	colorShader = new Shader(import(gl2~"shader_color.glsl"));
+	textureShader = new Shader(import(gl2~"shader_texture.glsl"));
+	circleShader = new Shader(import(gl2~"shader_circle.glsl"));
+	dashedShader = new Shader(import(gl2~"shader_dashed.glsl"));
 }
 
 void renderPoly(Shader shader, RawPolygon polygon) {
